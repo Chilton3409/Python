71e8404e29e5d5b9bcfa7898c7eb883a4c4e5bf2
GallbeGalled <59102857+Chilton3409@users.noreply.github.com>
2023-03-07 22:06:59 -0600

    Add files via upload

--- moving_files.py
+++ moving_files.py
@@ -5,6 +5,7 @@ import shutil
 from multiprocessing import Pool
 import argparse
 
+
 class Moving():
     def __init__(self):
         self.arguments = argparse.ArgumentParser(description="requests package",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -12,25 +13,36 @@ class Moving():
         
 
     def move_file(self, src, dst):
-        shutil.copy(src=input('Enter a file to copy'), dst=input('enter file destination'))
+        shutil.copy(src=input('Enter a file to copy\n'), dst=input('enter file destination\n'))
         print("your file {} has been moved to {}".format(str(src), str(dst)))
 
         return 
     def copyall(self, src):
         shutil.copytree(src=input('Enter directory to copy\n'), dst=input('direcory\n'), dirs_exist_ok=True)
 
+    def removedir(self):
+        shutil.rmtree(path=input('enter path to file to delete'))
+
     def makezip(self):
         shutil.make_archive(base_name=input("enter name for new zip file\n"), root_dir=input('Enter root or starting point\n'), format='zip')
 
 
     def arg(self):
-        self.arguments.add_argument('-z', '--zip', action='store_true', help='helper text') 
+        self.arguments.add_argument('-z', '--zip', action='store_true', help='Enter new filename, then enter root dir to start.') 
+        self.arguments.add_argument('-c', '--copy', action='store_true', help='enter src, then enter destination')
+        self.arguments.add_argument('-d', '--delete', action='store_true', help="Enter filepath for deletion")
 
         args = self.arguments.parse_args()
 
         if args.zip:
             self.makezip()
+
+        if args.copy:
+            self.move_file()
         
+        if args.delete:
+            self.removedir()
+
 def main():
     x = Moving()
     x.arg()
